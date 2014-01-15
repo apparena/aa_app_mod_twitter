b7bbda81c579c06b8c9a42b1ceb3b28299b05710
Marcus Merchel <kontakt@marcusmerchel.de>
2014-01-15 14:00:05 +0100

    GNR-3 - optimized code with abstact classes

--- js/models/LoginModel.js
+++ js/models/LoginModel.js
@@ -1,57 +1,31 @@
 define([
+    'ModelExtend',
     'underscore',
     'backbone',
     'localstorage'
-], function (_, Backbone) {
+], function (Model, _, Backbone) {
     'use strict';
 
-    var namespace = 'twitterLogin',
-        Model, Init, Remove, Instance;
+    return function () {
+        Model.namespace = 'twitterLogin';
 
-    Model = Backbone.Model.extend({
-        localStorage: new Backbone.LocalStorage('AppArenaAdventskalenderApp_' + _.aa.instance.aa_inst_id + '_TwLoginData'),
+        Model.code = Backbone.Model.extend({
+            localStorage: new Backbone.LocalStorage('AppArenaAdventskalenderApp_' + _.aa.instance.aa_inst_id + '_TwLoginData'),
 
-        defaults: {
-            'id':          1,
-            'twid':        '',
-            'screen_name': '',
-            'email':       '',
-            'firstname':   '',
-            'lastname':    '',
-            'city':        '',
-            'avatar':      '',
-            'login_type':  'twuser'
-        }
-    });
-
-    Remove = function () {
-        _.singleton.model[namespace].unbind().remove();
-        delete _.singleton.model[namespace];
-    };
-
-    Init = function (init) {
-
-        if (_.isUndefined(_.singleton.model[namespace])) {
-            _.singleton.model[namespace] = new Model();
-        } else {
-            if (!_.isUndefined(init) && init === true) {
-                Remove();
-                _.singleton.model[namespace] = new Model();
+            defaults: {
+                'id':          1,
+                'twid':        '',
+                'screen_name': '',
+                'email':       '',
+                'firstname':   '',
+                'lastname':    '',
+                'city':        '',
+                'avatar':      '',
+                'login_type':  'twuser'
             }
-        }
+        });
 
-        return _.singleton.model[namespace];
-    };
-
-    Instance = function () {
-        return _.singleton.model[namespace];
-    };
-
-    return {
-        init:        Init,
-        model:        Model,
-        remove:      Remove,
-        namespace:   namespace,
-        getInstance: Instance
-    };
-});
+        return Model;
+    }
+})
+;
